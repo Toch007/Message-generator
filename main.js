d8613a756e47daa9feaf7d476d7a8a7e581c3efd
Toch007 <tochukwu.uche1@gmail.com>
2023-07-04 13:32:27 +0100

    Confussion as code is not being recognized

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const quotes = [
   "The journey of a thousand miles begins with a single step.",
   "In the middle of difficulty lies opportunity.",
   "The best way to predict the future is to create it.",
-]
+];
 
 // Then declare an array of authors to match the quotes
 const authors = [
@@ -18,33 +18,35 @@ const authors = [
   "Lao Tzu",
   "Albert Einstein",
   "Peter Drucker",
-]
+];
 
 // Also an array of themes to that goes inline with the quotes
 const themes = [
-  "perseverance",
-  "hard work",
-  "creativity",
-  "success",
-  "opportunity",
-  "growth",
-  "wisdom",
-]
+  "Perseverance",
+  "Hard work",
+  "Creativity",
+  "Success",
+  "Opportunity",
+  "Growth",
+  "Wisdom",
+];
 
 // Create a function to generate Random Index
-const randomIndex = array => {
-  Math.floor(Math.random() * array.length);
-}
+const randomIndex = (array) => Math.floor(Math.random() * array.length);
 
 // Create a function for Generating Random Inspirational Messages 
 const generateMessage =  () => {
   //generate random indices for quotes, authors and themes
-  const quotesIndex = randomIndex(quotes);
-  const authorsIndex = randomIndex(authors);
-  const themesIndex = randomIndex(themes);
+  const quoteIndex = randomIndex(quotes);
+  const authorIndex = randomIndex(authors);
+  const themeIndex = randomIndex(themes);
 
   // use random indices to select quote, authors and themes using array indexing
-  const quote = quotes[quotesIndex];
-  const author = authors[authorsIndex];
-  const themes = themes[themesIndex];  
+  const quote = quotes[quoteIndex];
+  const author = authors[authorIndex];
+  const theme = themes[themeIndex];
+  
+  return `Theme for the day: ${theme}\n\n"${quote}"\n- ${author}`
+
 }
+console.log(theme)
